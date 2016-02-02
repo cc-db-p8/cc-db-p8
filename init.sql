0e687671b540b471f05a20f51049a0efd68abd18
paul guichon <paul.guichon@iedparis8.net>
2016-02-03 00:49:22 +0100

    correction premiere partie

--- init.sql
+++ init.sql
@@ -5,3 +5,20 @@ CREATE TABLE IF NOT EXISTS personne (
   date_naissance TEXT
 );
 
+CREATE TABLE IF NOT EXISTS adresse (
+  id INTEGER PRIMARY KEY ASC,
+  personne_id INTEGER,
+  label TEXT,
+  ville TEXT,
+  code_postal INT
+);
+
+
+INSERT INTO personne (id, nom, prenom, date_naissance) VALUES (NULL , 'Guichon', 'Paul', NULL );
+INSERT INTO adresse (id, personne_id, label, ville, code_postal) VALUES
+  (NULL, 1, '2 rue de la liberté', 'st denie', '93500');
+
+INSERT INTO adresse (id, personne_id, label, ville, code_postal) VALUES
+  (NULL, 1, '5 rue de la contrainte', 'Paris', '75016');
+
+SELECT * FROM adresse;
