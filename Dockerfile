64c5f7066c0af8ae8de11208c71e829bcac34df7
paul guichon <paul.guichon@iedparis8.net>
2016-02-03 12:29:16 +0100

    debut sqlite

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu:14.04
-RUN apt-get update && apt-get -y install sqlite3
+RUN apt-get update && apt-get -y install sqlite3 python
 ENV PYTHONUNBUFFERED 1
 RUN mkdir /code
 WORKDIR /code
--- cc-db-p8.py
+++ cc-db-p8.py
@@ -1,1 +1,20 @@
 # coding=utf-8
+
+import sqlite3
+conn = sqlite3.connect('exemple.db')
+
+c = conn.cursor()
+
+# c.execute("SELECT * FROM personne")
+#
+# print c.fetchall()
+#
+# query = [("toto {}".format(x), "truc {}".format(x),) for x in range(20)]
+# c.executemany('INSERT INTO personne (nom, prenom) VALUES (?, ?)', query)
+# conn.commit()
+#
+# c.execute("SELECT * FROM personne")
+#
+# print c.fetchall()
+#
+# conn.close()
